e1ed31601d39426458bd285f5d031c36e3556761
x41lakazam <x41lakazam@gmail.com>
2020-01-14 21:50:50 +0200

    Added class 22

--- Classes/class_22/DiProject/pyblog/__init__.py
+++ Classes/class_22/DiProject/pyblog/__init__.py
@@ -1,5 +1,17 @@
 import flask
+import os
+import flask_sqlalchemy
+import flask_migrate
 
 app = flask.Flask(__name__)
 
-from pyblog import routes
+
+basedir = os.path.abspath(os.path.dirname(__file__)) # Try to avoid hardcoding paths, use os
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'app.db')
+# This line is adding sqlite:/// with the path of your database
+
+db      = flask_sqlalchemy.SQLAlchemy(app)
+migrate = flask_migrate.Migrate(app, db)
+
+from pyblog import routes, models
